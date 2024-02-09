f81dcc5bd277ba38245a09f52c77dd6b69eae8cc
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-02-09 18:46:42 +0530

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -13,7 +14,10 @@ const port = process.env.PORT || 500;
 const app = express();
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,8 +1,9 @@
 import Router from "express";
-import { test } from "../controllers/user.controller.js";
+import { updateUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = Router();
 
-router.get("/", test);
+router.put("/update/:id", verifyToken, updateUser);
 
 export default router;
