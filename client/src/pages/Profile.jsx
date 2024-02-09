a55fa42eb31d21120f2f7360c26da586ad61ce1b
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-02-09 19:54:52 +0530

    complete update user functionalities

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   getDownloadURL,
   getStorage,
@@ -7,18 +7,24 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
   const fileRef = useRef(null);
 
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
 
   const [file, setFile] = useState(undefined);
   const [filePrec, setFilePrec] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   const [formData, setFormData] = useState({});
 
-  console.log(formData);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (file) {
@@ -55,10 +61,33 @@ const Profile = () => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -89,6 +118,7 @@ const Profile = () => {
           type="text"
           placeholder="userName"
           id="username"
+          defaultValue={currentUser.username}
           onChange={handleChange}
           className="border p-3 rounded-lg"
         />
@@ -96,31 +126,32 @@ const Profile = () => {
           type="text"
           placeholder="email"
           id="email"
+          defaultValue={currentUser.email}
           onChange={handleChange}
           className="border p-3 rounded-lg"
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
           id="password"
           onChange={handleChange}
           className="border p-3 rounded-lg"
         />
-        <input
-          type="text"
-          placeholder="confirm password"
-          id="confirmPassword"
-          onChange={handleChange}
-          className="border p-3 rounded-lg"
-        />
-        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-85">
-          Update
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-85"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
         <span className="text-red-700 cursor-pointer">Delete Account</span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      {error && <p className="text-red-700 mt-5">{error}</p>}
+      {updateSuccess && (
+        <p className="text-green-700 mt-5">User is updated successfully</p>
+      )}
     </div>
   );
 };
