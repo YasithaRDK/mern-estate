ecc6111c2cb517329a2a94986ee294312987a0be
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-02-14 00:17:14 +0530

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
 connectDB();
 
 const port = process.env.PORT || 500;
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,6 +30,12 @@ app.listen(port, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
